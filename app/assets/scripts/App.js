5c4310cda8262dba3c434387ca9be2f7fcaf02c8
Gaston K <gaston.kirsman@gmail.com>
2016-12-25 11:03:24 +0200

    Pre modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 
 //1er argumento el item a revelar, el 2do el offset
 new RevealOnScroll($('.feature-item'), "85%"); //Para features
 new RevealOnScroll($('.testimonial'), "60%"); //Para testimonials
+var stickyHeader = new StickyHeader();
